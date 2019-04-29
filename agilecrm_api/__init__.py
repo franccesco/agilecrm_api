43e5417c2372975e47e20695736b6aa59d0178d1
Franccesco Orozco <franccesco@codingdose.info>
2019-04-29 16:23:16 -0600

    remove hard-coded API keys from source and move them to .env for security

--- agilecrm_api/__init__.py
+++ agilecrm_api/__init__.py
@@ -1,1 +1,4 @@
+from .agilecrm import agileCRM, show_credentials
+
 __version__ = '0.1.0'
+__all__ = ['agileCRM', 'show_credentials']
--- agilecrm_api/agilecrm.py
+++ agilecrm_api/agilecrm.py
@@ -1,9 +1,14 @@
-import requests
 import json
+import requests
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
 
-APIKEY = "***********"   # Your API KEY
-EMAIL = "sample@agilecrm.com"  # Your API EMAIL
-DOMAIN = "sample"  # Your DOMAIN
+# Loading environment variables
+load_dotenv(find_dotenv())
+
+API_KEY = getenv('API_KEY')
+EMAIL = getenv('EMAIL')
+DOMAIN = getenv('DOMAIN')
 API_URL = f'https://" + {DOMAIN} + ".agilecrm.com/dev/api/'
 
 
@@ -11,25 +16,30 @@ def agileCRM(path, http_method, data, content_type):
 
     url = API_URL + path
     headers = {'Accept': 'application/json', 'content-type': content_type}
+    creds = EMAIL, API_KEY
 
     if http_method == "GET":
-        response = requests.get(url, headers=headers, auth=(EMAIL, APIKEY))
+        response = requests.get(url, headers=headers, auth=creds)
         return response.text
 
     if http_method == "POST":
-        response = requests.post(url, data=json.dumps(data), headers=headers, auth=(EMAIL, APIKEY))
+        response = requests.post(url, data=json.dumps(data), headers=headers, auth=creds)
         return response.text
 
     if http_method == "PUT":
-        response = requests.put(url, data=json.dumps(data), headers=headers, auth=(EMAIL, APIKEY))
+        response = requests.put(url, data=json.dumps(data), headers=headers, auth=creds)
         return response.text
 
     if http_method == "DELETE":
-        response = requests.delete(url, headers=headers, auth=(EMAIL, APIKEY))
+        response = requests.delete(url, headers=headers, auth=creds)
         return response
 
     if http_method == "POSTFORM":
-        response = requests.post(url, data=data, headers=headers, auth=(EMAIL, APIKEY))
+        response = requests.post(url, data=data, headers=headers, auth=creds)
         return response.text
 
     raise ValueError(f'Wrong HTTP Method: {http_method}')
+
+
+def show_credentials():
+    print(API_KEY, EMAIL, DOMAIN)
