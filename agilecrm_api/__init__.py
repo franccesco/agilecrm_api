90aaefcce6f390b5e6024624f29aba9966af33c9
Franccesco Orozco <franccesco@codingdose.info>
2019-05-10 13:10:03 -0600

    bump version number

--- agilecrm_api/__init__.py
+++ agilecrm_api/__init__.py
@@ -1,4 +1,4 @@
-from .agilecrm import agileCRM, show_credentials
+from .agilecrm import agileCRM
 
-__version__ = '1.0.1'
-__all__ = ['agileCRM', 'show_credentials']
+__version__ = '1.0.2'
+__all__ = ['agileCRM']
